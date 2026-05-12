d61add43164f2b756ec83a2b36308c182fda6f5f
krixPixel0o <aniruddh.bellur.03@gmail.com>
2026-05-12 22:26:21 +0530

    Done

--- app.py
+++ app.py
@@ -1,14 +1,6 @@
 from flask import Flask, render_template, request
 import mysql.connector
 
-d = mysql.connector.connect (
-    host = "localhost",
-    user = "root",
-    password = "krishkunt#24",
-    database = "portoDB"
-)
-
-c = d.cursor()
 
 a = Flask(__name__)
 
