bc4def656013e1ea131a20fc8906b530760288b4
krixPixel0o <aniruddh.bellur.03@gmail.com>
2026-05-12 21:59:19 +0530

    Enhanced portfolio UI and backend

--- static/js/script.js
+++ static/js/script.js
@@ -3,8 +3,7 @@ console.log("Website Portfolio has been loaded and is running !");
 const projButton = document.querySelector("button");
 
 projButton.addEventListener("click", function () {
-    const projectSection =
-    document.querySelector("#projects");
+    const projectSection = document.querySelector("#project-btn");
 
     projectSection.scrollIntoView({
         behavior: "smooth"
